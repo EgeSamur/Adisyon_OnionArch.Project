462eb2c2c374a497b6e5253fda97a351a223cde2
Ege Samur <egerumas@gmail.com>
2024-09-04 17:55:02 +0300

    dev prod ayrımı

--- src/Presentation/Adisyon_OnionArch.Project.Api/Program.cs
+++ src/Presentation/Adisyon_OnionArch.Project.Api/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// launch settimgs'in ASPNETCORE_ENVIROMENT'ýný kontrol
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
